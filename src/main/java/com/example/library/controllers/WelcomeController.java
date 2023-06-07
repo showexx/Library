4c91a00f7add0090186330575e60faee1f45ff7d
showex <official.showex@gmail.com>
2023-06-07 22:10:18 +0300

    create and customize authentication

--- src/main/java/com/example/library/controllers/WelcomeController.java
+++ src/main/java/com/example/library/controllers/WelcomeController.java
@@ -3,15 +3,13 @@ package com.example.library.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api")
-public class HelloController {
+@RequestMapping("/api/v1")
+public class WelcomeController {
 
-    @ResponseBody
-    @GetMapping("/sayHello")
-    public String sayHello(){
-        return "Hello World";
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
     }
 }
---
+++ src/main/java/com/example/library/services/AuthenticationService.java
@@ -0,0 +1,43 @@
+package com.example.library.services;
+
+import com.example.library.models.User;
+import com.example.library.models.enums.Role;
+import com.example.library.repositories.UserRepository;
+import com.example.library.requests.AuthenticationRequest;
+import com.example.library.requests.RegisterRequest;
+import com.example.library.responses.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ROLE_USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+}
